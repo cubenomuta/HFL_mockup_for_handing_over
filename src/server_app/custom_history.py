f84578c171aaa869730184caef468fba6c33e9cd
cubenomuta <037takunomu.ya@gmail.com>
2024-09-13 00:24:16 +0900

    ラウンドごとにjsonへ吐き出すように変更

--- src/server_app/custom_history.py
+++ src/server_app/custom_history.py
@@ -1,15 +1,22 @@
 # Flwoer API
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Optional
+from pathlib import Path
+import json
 
 from flwr.common.typing import Scalar
 from flwr.server.history import History
+
+from logging import DEBUG, INFO
+from flwr.common.logger import log
 
 
 class CustomHistory(History):
     """History class for training and/or evaluation metrics collection."""
 
-    def __init__(self) -> None:
+    def __init__(self, save_dir: Optional[str] = None) -> None:
         super(CustomHistory, self).__init__()
+        self.save_dir = save_dir
+        self.metrics_cluster_model: Dict[str, List[Tuple[int, Scalar]]] = {}
         self.timestamps_centralized: Dict[str, List[Tuple[int, Scalar]]] = {}
         self.timestamps_distributed: Dict[str, Dict[str, List[Tuple[int, Scalar]]]] = {}
 
@@ -28,6 +35,42 @@ class CustomHistory(History):
                 self.metrics_distributed[key][cid].append(
                     (server_round, metrics[key][cid])
                 )
+
+        losses_distributed = sorted(self.metrics_distributed["loss"].items())
+        save_path = Path(self.save_dir) / "metrics" / "losses_distributed.json"
+        with open(save_path, "w") as f:
+            json.dump(losses_distributed, f)
+
+        # accuracy of client models
+        accuracies_distributed = sorted(self.metrics_distributed["accuracy"].items())
+        save_path = Path(self.save_dir) / "metrics" / "accuracies_distributed.json"
+        with open(save_path, "w") as f:
+            json.dump(accuracies_distributed, f)
+
+    def add_metrics_cluster_models(
+        self, server_round: int, metrics: Dict[str, Dict[int, Scalar]]
+    ) -> None:
+        for key in metrics:
+            log(INFO, "Adding metrics for key: %s", key)
+            if key not in self.metrics_cluster_model:
+                self.metrics_cluster_model[key] = {}
+            for fid in metrics[key]:
+                for clsid in metrics[key][fid]:
+                    if clsid not in self.metrics_cluster_model[key]:
+                        self.metrics_cluster_model[key][clsid] = []
+                    self.metrics_cluster_model[key][clsid].append(
+                        (server_round, metrics[key][fid][clsid])
+                    )
+
+        losses_fog_centralized = sorted(self.metrics_cluster_model["loss"].items())
+        save_path = Path(self.save_dir) / "metrics" / "losses_cluster_model.json"
+        with open(save_path, "w") as f:
+            json.dump(losses_fog_centralized, f)
+
+        accuracies_fog_centralized = sorted(self.metrics_cluster_model["accuracy"].items())
+        save_path = Path(self.save_dir) / "metrics" / "accuracies_cluster_model.json"
+        with open(save_path, "w") as f:
+            json.dump(accuracies_fog_centralized, f)
 
     def add_timestamps_centralized(
         self, server_round: int, timestamps: Dict[str, Scalar]
@@ -51,3 +94,15 @@ class CustomHistory(History):
             self.timestamps_distributed[key]["comp"].append(
                 (server_round, timestamps[key]["comp"])
             )
+    
+    def save_loss_centralized(self):
+        losses_centralized = sorted(self.losses_centralized)
+        save_path = Path(self.save_dir) / "metrics" / "losses_centralized.json"
+        with open(save_path, "w") as f:
+            json.dump(losses_centralized, f)
+
+    def save_metrics_centralized(self):
+        accuracies_centralized = sorted(self.metrics_centralized["accuracy"])
+        save_path = Path(self.save_dir) / "metrics" / "accuracies_centralized.json"
+        with open(save_path, "w") as f:
+            json.dump(accuracies_centralized, f)
