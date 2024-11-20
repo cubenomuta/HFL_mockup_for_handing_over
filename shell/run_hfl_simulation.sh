a2e333e6a218f23d226ef7d177c5a2b7339daaf0
cubenomuta <037takunomu.ya@gmail.com>
2024-11-20 19:52:29 +0900

    実行用

--- shell/run_hfl_simulation.sh
+++ shell/run_hfl_simulation.sh
@@ -11,22 +11,22 @@ fi
 
 # fl configuration
 strategy="F2MKD"
-server_model="tinyCNN"
+server_model="GNResNet18"
 # client_model="tinyCNN"
-client_models="tinyCNN,tinyCNN_1conv" # csv形式
-dataset="OrganAMNIST"
-target="noniid-label2_part-noniid_0.2"
+client_models="MobileNetV2,tinyCNN" # csv形式
+dataset="CIFAR10"
+target="iid_iid"
 num_rounds=300
-num_fogs=5
-num_clients=100
+num_fogs=3
+num_clients=50
 fraction_fit=1
 
 # fit configuration
-yaml_path="./conf/${dataset}/${strategy}_${server_model}_tinyCNN/fit_config.yaml" # とりま固定値
+yaml_path="./conf/${dataset}/${strategy}_${server_model}_${client_models}/fit_config.yaml"
 seed=1234
 
 time=`date '+%Y%m%d%H%M'`
-exp_dir="./simulation/${dataset}/f_${num_fogs}_c${num_clients}_${target}/${strategy}_${server_model}_${client_models}/run_${time}"
+exp_dir="./simulation/${dataset}/f_${num_fogs}_c_${num_clients}_${target}/${strategy}_${server_model}_${client_models}/run_${time}"
 data_dir="./data/${dataset}/partitions/${target}/client"
 
 if [ ! -e "${exp_dir}" ]; then
@@ -43,7 +43,7 @@ python ./local/create_client_model_name_dict.py \
 --save_dir ${data_dir} \
 --seed ${seed} 
 
-ray start --head --min-worker-port 20000 --max-worker-port 29999 --num-cpus 5
+ray start --head --min-worker-port 20000 --max-worker-port 29999 --num-cpus 20 --num-gpus 8
 sleep 1 
 
 python ./local/hfl_simulation.py \
